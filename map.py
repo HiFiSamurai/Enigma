ec9f0f95c897e6fbd36f9561c57117d95130762e
jimitee <jimi.timmons@gmail.com>
2016-09-22 22:13:44 -0400

    charMap update
    removed numeric and special characters to match 26 character latin
    alphabet used in enigma machine (should be uppercase but whatever...
    change later)

--- map.py
+++ map.py
@@ -1,2 +1,2 @@
-charMap = [u'+', u'2', u'F', u'm', u'[', u'R', u';', u'z', u's', u'E', u',', u'H', u'U', u'^', u'6', u']', u"'", u':', u'Q', u'\\', u'0', u'k', u'V', u'T', u'8', u'w', u'C', u'h', u'S', u'B', u'Z', u'K', u'c', u')', u'!', u'r', u'=', u'*', u'$', u'Y', u'(', u'A', u'q', u'~', u'o', u'#', u'p', u'3', u'j', u'"', u'x', u'L', u'v', u'G', u'1', u'>', u'5', u'M', u'i', u'9', u'.', u'4', u'W', u'l', u'P', u'?', u'g', u'X', u'{', u't', u'-', u'&', u'`', u'7', u'_', u'/', u'@', u'f', u'u', u'J', u'O', u'n', u'd', u'e', u' ', u'a', u'N', u'<', u'|', u'}', u'%', u'y', u'I', u'b', u'D']
+charMap = [u'a', u'b', u'c', u'd', u'e', u'f', u'g', u'h', u'i', u'j', u'k', u'l', u'm', u'n', u'o', u'p', u'q', u'r', u's', u't', u'u', u'v', u'w', u'x', u'y', u'z']
 gears = [2.25, 1.125, 3.1, 4.2]
