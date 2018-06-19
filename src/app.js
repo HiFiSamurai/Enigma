67cb0d8e3d54849c149639451e9fe0b041866636
Maynard Russell <maynard.russell@gmail.com>
2018-06-18 22:52:03 -0400

    Update editor and lint config, minor spacing tweaks

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import View from '@HiFiSamurai/ui-toolkit/dist/js/view';
+import {View} from '@HiFiSamurai/ui-toolkit';
 import html from './app.html';
 import './app.scss';
 
@@ -7,17 +7,17 @@ import Enigma from './machine/Enigma';
 const enigma = new Enigma();
 
 class App extends View {
-  static get name() { return 'enigma-app'; }
-  static get html() { return html; }
+    static get name() { return 'enigma-app'; }
+    static get html() { return html; }
 
-  connectedCallback() {
-    this.querySelector('#submitTrigger').addEventListener('click', () => {
-      const inputText = this.querySelector('#inputText').value;
-      const mode = this.querySelector('#machineMode').checked ? 'decode' : 'encode';
-      const outputText = enigma[mode](inputText);
-      this.querySelector('#outputText').innerHTML = outputText;
-    });
-  }
+    connectedCallback() {
+        this.querySelector('#submitTrigger').addEventListener('click', () => {
+            const inputText = this.querySelector('#inputText').value;
+            const mode = this.querySelector('#machineMode').checked ? 'decode' : 'encode';
+            const outputText = enigma[mode](inputText);
+            this.querySelector('#outputText').innerHTML = outputText;
+        });
+    }
 };
 
 App.wrapped();
